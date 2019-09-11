489b32b3fe8765827f971ce7eec224288d93b4eb
Jorge Viana <secoviana@gmail.com>
2019-09-11 22:46:56 +0200

    testEquality - green (fake implementation)

--- src/main/java/io/github/jorgeviana/money/Dollar.java
+++ src/main/java/io/github/jorgeviana/money/Dollar.java
@@ -10,4 +10,9 @@ class Dollar {
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object other) {
+        return true;
+    }
 }
