c5d1a6caaf58ffb4d29725b1e7714b29ef2deaf2
Jorge Viana <secoviana@gmail.com>
2019-09-12 10:21:44 +0200

    moving equals from Dollar to Money

--- src/main/java/io/github/jorgeviana/money/Dollar.java
+++ src/main/java/io/github/jorgeviana/money/Dollar.java
@@ -9,10 +9,4 @@ class Dollar extends Money {
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object other) {
-        Money money = (Money) other;
-        return amount == money.amount;
-    }
 }
--- src/main/java/io/github/jorgeviana/money/Money.java
+++ src/main/java/io/github/jorgeviana/money/Money.java
@@ -2,4 +2,10 @@ package io.github.jorgeviana.money;
 
 class Money {
     protected int amount;
+
+    @Override
+    public boolean equals(Object other) {
+        Money money = (Money) other;
+        return amount == money.amount;
+    }
 }
