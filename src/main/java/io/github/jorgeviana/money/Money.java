c2cb0fe09d58f97b203ee14effab48848cf4e436
Jorge Viana <secoviana@gmail.com>
2019-09-12 13:30:54 +0200

    Franc constructor assigning parameter to instance variable

--- src/main/java/io/github/jorgeviana/money/Money.java
+++ src/main/java/io/github/jorgeviana/money/Money.java
@@ -9,7 +9,7 @@ abstract class Money {
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, null);
+        return new Franc(amount, "CHF");
     }
 
     abstract Money times(int amount);
