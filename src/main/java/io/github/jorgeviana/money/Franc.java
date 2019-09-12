cda70bcf17417f2a1786fd3b79a2b6ff5686acf4
Jorge Viana <secoviana@gmail.com>
2019-09-12 10:27:02 +0200

    removing equals in Franc

--- src/main/java/io/github/jorgeviana/money/Franc.java
+++ src/main/java/io/github/jorgeviana/money/Franc.java
@@ -9,10 +9,4 @@ class Franc extends Money {
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object other) {
-        Money money = (Money) other;
-        return amount == money.amount;
-    }
 }
