e303e270ff922217494796017c24cd8c7e4494ec
Jorge Viana <secoviana@gmail.com>
2019-09-12 11:17:38 +0200

    towards eliminating subclasses

--- src/main/java/io/github/jorgeviana/money/Money.java
+++ src/main/java/io/github/jorgeviana/money/Money.java
@@ -3,6 +3,10 @@ package io.github.jorgeviana.money;
 class Money {
     protected int amount;
 
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object other) {
         Money money = (Money) other;
